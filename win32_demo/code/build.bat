3f9bbec3d43eaad51849825de7b1d2c7cf0d2343
wheresthecat <frosty@centrum.cz>
2022-08-28 22:39:20 +0200

    First steps.

--- win32_demo/code/build.bat
+++ win32_demo/code/build.bat
@@ -4,11 +4,12 @@ REM Building stuff
 REM Remember to type "shell" to set up the environement for msvs
 
 set CompilerFlags=-Zi -nologo
+set LinkerFlags= user32.lib gdi32.lib
 
 IF NOT EXIST ..\..\build mkdir ..\..\build
 
 pushd ..\..\build
 
-cl %CompilerFlags% ..\win32_demo\code\win32_demo.cpp
+cl %CompilerFlags% ..\win32_demo\code\win32_demo.cpp %LinkerFlags%
 
 popd
--- win32_demo/code/win32_demo.cpp
+++ win32_demo/code/win32_demo.cpp
@@ -1,5 +1,7 @@
 #include <windows.h>
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 int CALLBACK WinMain(
   HINSTANCE hInstance,
   HINSTANCE hPrevInstance,
@@ -8,4 +10,29 @@ int CALLBACK WinMain(
 )
 {
     return(0);
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch(uMsg)
+    {
+        case WM_DESTROY:
+            {
+                PostQuitMessage(0);
+                return 0;
+            }
+
+        case WM_PAINT:
+            {
+                PAINTSTRUCT ps;
+                HDC hdc = BeginPaint(hwnd, &ps);
+
+                FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+
+                EndPaint(hwnd, &ps);
+            }
+            return(0);
+    }
+
+    return(DefWindowProc(hwnd, uMsg, wParam, lParam));
 }
