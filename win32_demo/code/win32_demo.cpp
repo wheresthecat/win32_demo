70c29d2f6ad1baca12472aee3b18cd31cb2b29e2
wheresthecat <frosty@centrum.cz>
2022-08-29 22:12:26 +0200

    Update win32_demo.cpp

--- win32_demo/code/win32_demo.cpp
+++ win32_demo/code/win32_demo.cpp
@@ -1,38 +1,115 @@
 #include <windows.h>
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
-int CALLBACK WinMain(
-  HINSTANCE hInstance,
-  HINSTANCE hPrevInstance,
-  LPSTR     lpCmdLine,
-  int       nShowCmd
-)
+LRESULT CALLBACK
+MainWindowCallback(HWND Window, UINT Message, WPARAM wParam, LPARAM lParam)
 {
-    return(0);
-}
+    LRESULT Result = 0;
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch(uMsg)
+    switch(Message)
     {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+        
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+
         case WM_DESTROY:
-            {
-                PostQuitMessage(0);
-                return 0;
-            }
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
 
         case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            static DWORD Operation = WHITENESS;            
+            PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+            if(Operation == WHITENESS)
             {
-                PAINTSTRUCT ps;
-                HDC hdc = BeginPaint(hwnd, &ps);
-
-                FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
-
-                EndPaint(hwnd, &ps);
+                Operation = BLACKNESS;
             }
-            return(0);
+            else
+            {
+                Operation = WHITENESS;
+            }
+            EndPaint(Window, &Paint);
+        } break;
+        
+        default:
+        {
+            Result = DefWindowProc(Window, Message, wParam, lParam);
+        } break;
     }
 
-    return(DefWindowProc(hwnd, uMsg, wParam, lParam));
+    return(Result);
+}
+
+int CALLBACK
+WinMain(HINSTANCE Instance,
+        HINSTANCE PrevInstance,
+        LPSTR CommandLine,
+        int ShowCode)
+{
+    WNDCLASS WindowClass = {};
+
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    WindowClass.lpszClassName = "Win32DemoClass";
+
+    if(RegisterClassA(&WindowClass))
+    {
+        HWND WindowHandle = 
+            CreateWindowExA(0,
+                            WindowClass.lpszClassName,
+                            "Win32_Demo",
+                            WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                            CW_USEDEFAULT,
+                            CW_USEDEFAULT,
+                            CW_USEDEFAULT,
+                            CW_USEDEFAULT,
+                            0,
+                            0,
+                            Instance,
+                            0);
+        if(WindowHandle)
+        {
+            for(;;)
+            {
+                MSG Message;
+                BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
+                if(MessageResult > 0)
+                {
+                    TranslateMessage(&Message);
+                    DispatchMessageA(&Message);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // Logging
+        }
+    }
+    else
+    {
+        // Logging
+    }
+
+    return(0);
 }
