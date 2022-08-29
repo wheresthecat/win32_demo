166db9cc9e94b58f2fa61fdfcad70a077f0de79d
wheresthecat <frosty@centrum.cz>
2022-08-29 08:22:36 +0200

    Update fakedrive.bat
    fixed typo

--- win32_demo/misc/fakedrive.bat
+++ win32_demo/misc/fakedrive.bat
@@ -1,5 +1,5 @@
 @echo off
-REM This will create a fake drive letter so you can put this project whenever you want
+REM This will create a fake drive letter so you can put this project wherever you want
 REM without messing with paths and stuff.
 REM Just change the path of a subst and you're all done.
 REM And put it in a startup folder "win + r" and shell:commont startup or make a PATH thing
